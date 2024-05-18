5cf56430d106a844f7f10ac13a388175dbb6da96
dk10119 <tranduclong092@gmail.com>
2024-05-18 09:59:56 +0300

    Feat: update the main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,11 +8,20 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    #[Route('/', name: 'app_main')]
+    public function homepage(): Response
     {
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip
         ]);
     }
 }
